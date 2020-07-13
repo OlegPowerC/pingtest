0f1029c909bcd5bc5dfe6d08141ab5d88b0bc72a
Oleg <oleg@powerc.ru>
2020-07-13 11:13:11 +0300

    Add range in IP addresses

--- pingtest.py
+++ pingtest.py
@@ -15,17 +15,37 @@ if __name__ == '__main__':
     StringZ = "Datetime: " + CDate + "\n"
     f.write(StringZ)
     for Server in IPaddrs:
-        response = ping(Server["IP"])
-        Reach = "Err"
-        if response.success():
-            Reach = "Ok"
-            ServerString = Server["Name"]+" "+ Server["IP"] + " " + Reach + "\n"
-            print(ServerString)
-            f.write(ServerString)
-        else:
-            Reach = "Error"
-            ServerString = Server["Name"] + " " + Server["IP"] + " " + Reach + "\n"
-            f.write(ServerString)
-            print(ServerString)
+        if "IP" in Server.keys():
+            Ip = Server["IP"].split("-")
+            IpAddress = []
+            if len(Ip) == 2:
+                IpOctetsFirst = Ip[0].split(".")
+                if len(IpOctetsFirst) < 4:
+                    break
+                lastdigitinipfirst = IpOctetsFirst[3]
+                lastipaddrlastdigit = Ip[1]
+                for Digit1 in range(int(lastdigitinipfirst),int(lastipaddrlastdigit)):
+                    IpAddrTmp = "%s.%s.%s.%d"%(IpOctetsFirst[0],IpOctetsFirst[1],IpOctetsFirst[2],Digit1)
+                    IpAddress.append(IpAddrTmp)
+
+            else:
+                IpAddress.append(Server["IP"])
+
+            print(IpAddress)
+
+
+            for IpAddressEach in IpAddress:
+                response = ping(IpAddressEach)
+                Reach = "Err"
+                if response.success():
+                    Reach = "Ok"
+                    ServerString = Server["Name"]+" "+ IpAddressEach + " " + Reach + "\n"
+                    print(ServerString)
+                    f.write(ServerString)
+                else:
+                    Reach = "Error"
+                    ServerString = Server["Name"] + " " + IpAddressEach + " " + Reach + "\n"
+                    f.write(ServerString)
+                    print(ServerString)
 
 
